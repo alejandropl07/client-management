5c504b195f7072c7f16746a595b41db6363c828a
alejandropl07 <aprietoleon97@gmail.com>
2022-06-01 00:19:38 -0500

    Config axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
-import {StateProvider} from './StateProvider';
+import "./App.css";
+import { StateProvider } from "./StateProvider";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Registration from "./components/Registration";
+import Error from "./components/Error";
 
 function App() {
   return (
-    <StateProvider>
-    <div>
-      adsasdsads
-    </div>
-    </StateProvider>
+    <Router>
+      <StateProvider>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/registration" element={<Registration />} />
+          <Route exact path="/error" element={<Error />} />
+        </Routes>
+      </StateProvider>
+    </Router>
   );
 }
 
