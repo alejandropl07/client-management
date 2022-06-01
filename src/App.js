0dc1053509990aaa9251229178a9c6da6f756b3e
alejandropl07 <aprietoleon97@gmail.com>
2022-05-31 23:47:24 -0500

    Context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {StateProvider} from './StateProvider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <StateProvider>
+    <div>
+      adsasdsads
     </div>
+    </StateProvider>
   );
 }
 
